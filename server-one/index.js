b4859662c420c8247537a08d58857e7884a0d0ab
asmita143 <89544722+asmita143@users.noreply.github.com>
2022-10-28 21:20:49 +0300

    public folder downloaded and static content is used and cat info is passed

--- server-one/index.js
+++ server-one/index.js
@@ -4,14 +4,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.static('public'));
 
-app.get('/test', (req, res) => {
-    res.send('It works.');
+app.get("/catinfo", (req, res) => {
+    const cat = {
+      name: "Frank",
+      birthdate: "2010-12-25",
+      weight: 5,
+    };
+    res.json(cat);
   });
-  
+
 app.listen(port, () => {
   console.log(`I am being used. ${port}`);
 });
