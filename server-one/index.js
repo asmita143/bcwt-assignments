68e4a5100d80d2716b47b561a0f64bd9d1feb0a6
asmita143 <89544722+asmita143@users.noreply.github.com>
2022-10-28 21:17:23 +0300

    express dependency is imported and initial test in local host is done and succeedede

---
+++ server-one/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/test', (req, res) => {
+    res.send('It works.');
+  });
+  
+app.listen(port, () => {
+  console.log(`I am being used. ${port}`);
+});
