d8a218b2e7e5192c92c9745ab8700df315028385
asmita143 <89544722+asmita143@users.noreply.github.com>
2022-11-01 15:00:59 +0200

    added basic dummy routing

--- week2/app.js
+++ week2/app.js
@@ -7,4 +7,25 @@ app.get('/cat', (req, res) => {
   res.send('From this endpoint you can get cats.')
 });
 
+app.get('/cat/:catId', (req, res) => {
+  console.log(req.params)
+  res.send('This one is for cat id!!.')
+});
+
+app.post('/cat', (req, res) => {
+  res.send('This one is for adding more cats!!.')
+});
+
+app.put('/cat', (req, res) => {
+  res.send('This one is for the editing cats.')
+});
+app.delete('/cat', (req, res) => {
+  res.send('This one is for deleting cats!!.')
+});
+
+// app.get('/cat', (req, res) => {
+// res.send('From this endpoint you can get cats.')
+// });
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
