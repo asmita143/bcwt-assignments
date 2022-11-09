85b442c25b02765d51b1edcdf365aecf3cc3460c
asmita143 <89544722+asmita143@users.noreply.github.com>
2022-11-09 18:08:47 +0200

    initiation of tasks

---
+++ front_end_task/task2-ajax/script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const apiUrl = " https://api.tvmaze.com/search/shows?q=";
+//get reference to form 
+
+const form =  document.querySelector('#search-form');
+const button = form.querySelector('button');
+const input = form.querySelector('input');
+const results = document.querySelector('#results');
+
+button.addEventListener('click', (event) => {
+    //do no submut the form to anywhere(no page refresh)
+    event.preventDefault();
+    //prevent the generic event listener at the bottom
+    event.stopPropagation();
+    const queryParam = input.value;
+    if(queryParam.length>1){
+        getTVSerialData(queryParam);
+
+    }
+});
+
+const renderResults = (data) => {
+    //clear existing results before appending new ones
+    results.innerHTML='';
+    for(let i=0; i<data.length;i++){
+        const resultText  =document.createElement('h3');
+        const img = document.createElement('img');
+        img.src = data[i].show.image.medium;
+        resultText.textContent= "{"+data[i].show.name +"} can be watched on : "+ data[i].show.officialSite; 
+        results.append(resultText);
+        results.append(img);
+    }
+};
+
+const getTVSerialData = async (name) =>{
+    try {
+        const response = await fetch(apiUrl+ name);
+        const data = await response.json();
+        console.log(data);
+        renderResults(data);
+        
+    } catch (error) {
+        
+    }
+   
+}
---
+++ front_end_task/task2-ajax/script_dummy.js
@@ -0,0 +1,17 @@
+'use strict';
+console.log("hello");
+
+const figCaptionElem=document.querySelector('figcaption');
+const imgElem=document.querySelector('img');
+
+const getData = async () =>{
+    const response= await fetch("pics.json");
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    figCaptionElem.innerText=data[0].name;
+    imgElem.src=data[0].url;
+    imgElem.alt=data[0].description;
+};
+
+getData();
