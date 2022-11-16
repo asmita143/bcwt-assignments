2669a7b072cb4a5131d6319ffb05c9af3a56a796
asmita143 <89544722+asmita143@users.noreply.github.com>
2022-11-16 09:25:00 +0200

    Added more sanitizers

--- week2/routes/userRoute.js
+++ week2/routes/userRoute.js
@@ -1,18 +1,22 @@
-'use strict';
-const express=require('express');
-const router=express.Router();
-const{body}=require('express-validator')
-const userController=require('../controllers/userController');
+"use strict";
+const express = require("express");
+const router = express.Router();
+const { body } = require("express-validator");
+const userController = require("../controllers/userController");
 
-
-router.get('/', userController.getUsers)
-.get('/:userId', userController.getUser)
-.post('/',body('name').isLength({min: 3}), body('email').isEmail(), body('passwd').isLength({min:8}),
-userController.createUser)
-.put('/', (req, res) => {
-    res.send('This one is for the editing users.')
+router
+  .get("/", userController.getUsers)
+  .get("/:userId", userController.getUser)
+  .post(
+    "/",
+    body("name").isLength({ min: 3 }).trim().escape(),
+    body("email").isEmail().normalizeEmail(),
+    body("passwd").isLength({ min: 8 }).trim(),
+    userController.createUser
+  )
+  .put("/", (req, res) => {
+    res.send("This one is for the editing users.");
   })
-  .delete('/:userId',userController.deleteUser); 
+  .delete("/:userId", userController.deleteUser);
 
-
-module.exports=router;
+module.exports = router;
